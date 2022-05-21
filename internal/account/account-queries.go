f7ba209ed7dc4115eaf53890f1d9c66b18ef2c15
netivit yuraphan <netivitton@gmail.com>
2022-05-21 18:33:44 +0700

    update

--- internal/account/account-queries.go
+++ internal/account/account-queries.go
@@ -1,6 +1,8 @@
 package internal
 
 import (
+	"encoding/json"
+	"fmt"
 	"log"
 
 	"github.com/netivitton/go-rest-api/utils"
@@ -28,9 +30,22 @@ func InsertAccount() string {
 	}
 	tx := gormdb.Begin()
 	address := &Address{Home_no: "123"}
-	tx.Create(&address)
-	tx.Create(&Account{UserName: "AAA", Password: "BBB", AddressID: address.ID})
+	tx.Create(address)
+	tx.Create(&Account{UserName: "BBB", Password: "CCC", AddressID: address.ID})
 	tx.Commit()
+	utils.CloseDB(gormdb)
+	return "ta da! \n"
+}
+
+func LoginAccount() string {
+	gormdb, err := utils.Connection()
+	if err != nil {
+		log.Fatal("cannot connect DB", err)
+	}
+	var result Account
+	gormdb.Raw("SELECT id, created_at, updated_at, deleted_at, user_name, password, name, last_name, email, fa_code, address_id FROM accounts where user_name = ? AND password = ?;", "AAA", "BBB").Scan(&result)
+	marshal_struct, _ := json.Marshal(result)
+	fmt.Println("Marshaled string: ", string(marshal_struct))
 	utils.CloseDB(gormdb)
 	return "ta da! \n"
 }
